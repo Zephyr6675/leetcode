c533a2b9b76d6ddc07101f30b22a794e66909bac
Passang Tamang <tpassang541@gmail.com>
2026-06-10 17:35:25 +0530

    Time: 8 ms (10.4%), Space: 8.6 MB (65.14%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int reverse(int n){
+        int revNum=0;
+        while (n>0){
+            int digit = n%10;
+            if(revNum>INT_MAX/10 || revNum<INT_MIN/10){
+                return 0;
+            }
+            revNum=revNum*10 + digit;
+            n=n/10;
+        }
+        return revNum;
+    }
+
+    bool isPalindrome(int n) {
+        if (n<0){
+            return false;
+        }
+        if (n==reverse(n)){
+            return true;
+        }
+        else {
+            return false;
+        }
+        }
+      
+    
+};
